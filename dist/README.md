0612e4477f0917ef09d8ffdf4127c157f7393ead
Mario Kaack <mario@shortrope.com>
2015-08-27 13:57:19 -0700

    Initial Commit

---
+++ dist/README.md
@@ -0,0 +1,32 @@
+#Accordion.mak.js
+
+This JavaScript accordion is designed to allow any element be a source link to a target accordion element. You are also not restricted to a specific container.
+
+Source element must contain 'data-accordion-target' attribute:
+data-accordion-target="<id-of-target-element>" 
+
+The target element must have an 'id' that matches the 'data-accordion-target' value from the source link.
+
+The target element must also have this class:
+class="accordion-target"
+
+
+<p data-accordion-target="aTarget-1">Source element 1</p>
+<div id="aTarget-1" class="accordion-target">
+	<p>A bunch of whaterver</p>
+</div>
+
+<h3 data-accordion-target="aTarget-2">Source element 1</h3>
+<p id="aTarget-2" class="accordion-target">
+	A bunch of more whaterver
+</p>
+
+
+The transition time can be set in the accordion.mak.css file which has one selector w two properties:
+	.accordion-target { 
+	    overflow: hidden;
+	    transition:height 0.5s;
+	}
+
+
+
---
+++ js/accordion.mak.js
@@ -0,0 +1,96 @@
+(function () {
+
+    'use strict';
+
+    /* VARiables 
+    =============================================== */
+    var aSources,
+        aTargetIds,
+        aTargetHeights,
+        i;
+
+
+    /* FUNCTIONS 
+    =============================================== */
+
+    /* @return array of strings */
+    function getTargetIds(srcs) {
+        var targs = [];
+        for (i = 0; i < srcs.length; i += 1) {
+            targs.push(srcs[i].getAttribute('data-accordion-target'));
+        }
+        return targs;
+    }
+
+    /* Creates an array of target heights to be used for the transition
+     * of each individual target. The heights are collected before they
+     * are set to zero
+      @param targetIds:array
+      @return array
+     */
+    function getTargetHeights(targetIds) {
+        var heights = [],
+            elem;
+        for (i = 0; i < targetIds.length; i += 1) {
+            elem = document.getElementById(targetIds[i]);
+            heights.push(elem.clientHeight);
+        }
+        return heights;
+    }
+
+    /* Sets the target element height to zero 
+     * @param targetId:string
+     */
+    function zeroHeight(targetId) {
+        document.getElementById(targetId).style.height = '0';
+    }
+
+    /* Adds a 'click' listener to a Source element
+     * @param aSrc:object - source element
+     * @param tId:string - target Id
+     * @param tHeight:string - target height
+     * @param singleOpen:boolean - if true, only one target open at a time
+     *                           - if false, each click is a toggle for that element
+     */
+    function addAccordionListener(aSrc, tId, tHeight, singleOpen) {
+        aSrc.addEventListener('click', function (e) {
+            if (singleOpen) {
+                for (i = 0; i < aTargetIds.length; i += 1) {
+                    document.getElementById(aTargetIds[i]).style.height = '0';
+                }
+            }
+            var theTarget = document.getElementById(tId);
+            // if toggle open / closed state
+            if (theTarget.clientHeight === 0) {
+                theTarget.style.height = tHeight + 'px';
+            } else {
+                theTarget.style.height = '0';
+            }
+        });
+    }
+
+
+    /* APPLICATION 
+    =============================================== */
+
+    // get array of accordion source elements
+    aSources = document.querySelectorAll('[data-accordion-target]');
+
+    // get the accordion target ids from the sources
+    aTargetIds = getTargetIds(aSources);
+
+    // Get the height of each of the targets.
+    aTargetHeights = getTargetHeights(aTargetIds);
+
+    // Set height of all targets to zero
+    for (i = 0; i < aTargetIds.length; i += 1) {
+        zeroHeight(aTargetIds[i]);
+    }
+
+
+    // add click eventListener to each Source element
+    for (i = 0; i < aSources.length; i += 1) {
+        addAccordionListener(aSources[i], aTargetIds[i], aTargetHeights[i], true);
+    }
+
+})();
